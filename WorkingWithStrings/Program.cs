b86fbc0404db428c937d12e2ebb7219a623a7b68
Kseniya <kseniya-ignateva-2002@mail.ru>
2021-02-18 20:55:32 +0500

    Переименован файл TableOfFunctionValues, и изменения в программе

--- WorkingWithStrings/Program.cs
+++ WorkingWithStrings/Program.cs
@@ -8,32 +8,85 @@ namespace WorkingWithStrings
         {
             Console.WriteLine("Здравствуйте, введите какой-то текст.");
             string text = Console.ReadLine();
+            string[] specialTextArray = (text.ToLower()).Split(new char[] { '!', '-', '.', ',', ':', ';', '"', '(', ')', '?', ' ' }, StringSplitOptions.RemoveEmptyEntries); //разбиваем текст на отдельные слова
+            int max = 0;
+            int indexMaxWord = 0;
             ValuePunct(text);
-            BreakText(text);
+            BreakTextFor(text);
+            SpecialWordInText(specialTextArray);
+            TheLongestWordInTheText(specialTextArray, ref max, ref indexMaxWord);
+            evenOrOddLongWord(specialTextArray, max, indexMaxWord);
         }
         static void ValuePunct(string text)
         {
-            string[] temp = text.Split(" ", StringSplitOptions.RemoveEmptyEntries);
-            char[] punctuationMarks = new char[] { '!', '?', ';', '.', ',', '-', '"', '(', ')' };
-            int value = 0;
-            for (int i = 0; i < temp.Length; i++)
+            char[] textArray = text.ToCharArray();
+            char[] punctuationMarks = new char[] { '!', '-', '.', ',', ':', ';', '"', '(', ')', '?' };
+            int valuePunctuation = 0;
+            for (int i = 0; i < text.Length; i++)
             {
-                foreach (var item in punctuationMarks)
+                for (int j = 0; j < punctuationMarks.Length; j++)
                 {
-                    if (temp[i].Contains(item))
-                        value += 1;
+                    if (Convert.ToString(textArray[i]).Contains(punctuationMarks[j]))
+                        valuePunctuation++;
                 }
             }
-            Console.WriteLine(value);
+            Console.WriteLine("Кол-во знаков препинания в тексте: " + valuePunctuation);
         }
-        static void BreakText(string text)
-        { 
-            string[] breakString = text.Split('.','!', '?');
-            foreach (var item in breakString)
+        static void BreakTextFor(string text)
+        {
+            string[] breakText = text.Split(new char[] { '.', '!', '?' }, StringSplitOptions.RemoveEmptyEntries);
+            Console.WriteLine("Разбиваем текст на отдельные предложения:");
+            for (int i = 0; i < breakText.Length; i++)
+                Console.WriteLine(breakText[i]);
+        }
+        static void SpecialWordInText(string[] specialTextArray) //переписать
+        {
+            Console.Write("Уникальные слова в тексте: ");
+            //for (int i = 0; i < specialTextArray.Length; i++)
+            //{
+            //    int similarityValue = 0;
+            //    for (int j = 0; j < specialTextArray.Length; j++)
+            //    {
+            //        if (specialTextArray[i] != specialTextArray[j] && i != j)
+            //            similarityValue++;
+            //    }
+            //    if (similarityValue == (specialTextArray.Length - 1))
+            //        Console.Write(specialTextArray[i] + ", ");
+            //}
+            for (int i = 0; i < specialTextArray.Length; i++)
             {
-                Console.WriteLine(item);
+                for (int j = 0; j < specialTextArray.Length; j++)
+                {
+                    if (specialTextArray[i] == specialTextArray[j] && i != j)
+                        specialTextArray[i] = "";
+                }
+            }
+            Console.WriteLine(specialTextArray);
+        }
+        static void TheLongestWordInTheText(string[] specialTextArray, ref int max, ref int indexMaxWord)
+        {
+            for (int i = 0; i < specialTextArray.Length; i++)
+            {
+                if (specialTextArray[i].Length > max)
+                {
+                    max = specialTextArray[i].Length;
+                    indexMaxWord = i;
+                }
+            }
+            Console.WriteLine("");
+            Console.WriteLine("Самое длинное слово во всем тексте: " + specialTextArray[indexMaxWord]);
+        }
+        static void evenOrOddLongWord(string[] specialTextArray, int max, int indexMaxWord)
+        {
+            if (max % 2 == 0) // четное
+                Console.WriteLine(specialTextArray[indexMaxWord].Substring(max / 2));
+            else // нечетное
+            {
+                char[] longWord = specialTextArray[indexMaxWord].ToCharArray(); // раскладываем слово на массив букв
+                int middleOfTheWord = max / 2; //ищем индекс середины слова
+                longWord[middleOfTheWord] = '*';
+                Console.Write(longWord);
             }
         }
-        static void BreakText(string text)
     }
 }
