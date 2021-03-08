a1c85897c37eeb82e0a2e3d251182b238d321bf1
Kseniya <kseniya-ignateva-2002@mail.ru>
2021-03-08 14:21:35 +0500

    Добавлен метод Calculate считающий через ОПЗ

--- AdvancedCalculator/Program.cs
+++ AdvancedCalculator/Program.cs
@@ -8,27 +8,19 @@ namespace AdvancedCalculator
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Ваш пример должен начинаться с y=, а также дробные числа должны быть записаны через запятую. И пробелов в примере быть не должно. \nТеперь введите чему равен x.");
+            char valueX = Convert.ToChar(Console.ReadLine());
             WorkWithFiles workWithFile = new WorkWithFiles();
             SearchSomething Search = new SearchSomething();
             RPN rPN = new RPN();
             string functionTXT = workWithFile.ReadingTheFunctionFromTXT();
             char[] charsToTrim = { '=', 'y' };
-            string function = functionTXT.Trim(charsToTrim);
+            string function = functionTXT.Trim(charsToTrim).Replace('x', valueX);
             string[] numberInFunction = Search.SearchNumbersInTXT(function);
             int valueOperationsInBrace = Search.SearchValueOperationsInBrace(function);
             rPN.ParseExpression(numberInFunction, function, valueOperationsInBrace);
-
-            //Console.WriteLine("Введите шаг построения (значение, на которое увеличивает X).");
-            //int buildStep = Convert.ToInt32(Console.ReadLine());
-            //Console.WriteLine("X изначально равен 0. Введите диапазон значений X.");
-            //int rangeOfValues = Convert.ToInt32(Console.ReadLine());
-            //Console.Clear();
-            //string[,] tableOfFunctionValues = new string[rangeOfValues + 1, 2]; // rangeOfValues + (ячейки под Х и У)
-            //TableValues(tableOfFunctionValues, rangeOfValues, buildStep);
-            //int maxLenght = 0;
-            //MaxNumber(tableOfFunctionValues, rangeOfValues, maxLenght);
-            ////DrawTable(rangeOfValues, maxLenght);
-            //FillTableFigures(tableOfFunctionValues, rangeOfValues);
+            object[] result = rPN.Calculate().ToArray();
+            Console.WriteLine(result[0]);
         }
     }
 }
