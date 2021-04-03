a754b3df02bec5fd7e4e1780686d3e0793a50bd7
Kseniya <kseniya-ignateva-2002@mail.ru>
2021-04-03 20:27:14 +0500

    рефакторинг

--- AdvancedCalculator/Program.cs
+++ AdvancedCalculator/Program.cs
@@ -9,21 +9,19 @@ namespace AdvancedCalculator
         static void Main(string[] args)
         {
             Console.WriteLine("Ваш пример должен начинаться с y=, а также дробные числа должны быть записаны через запятую. И пробелов в примере быть не должно. \nТеперь введите чему равен x.");
-            char valueX = Convert.ToChar(Console.ReadLine());
+            string valueX = Console.ReadLine();
             WorkWithFiles workWithFile = new WorkWithFiles();
             SearchSomething Search = new SearchSomething();
             RPN rPN = new RPN();
             string functionTXT = workWithFile.ReadingTheFunctionFromTXT();
             char[] charsToTrim = { '=', 'y' };
-            string function = functionTXT.Trim(charsToTrim).Replace('x', valueX);
+            string function = functionTXT.Trim(charsToTrim).Replace("x", valueX);
             string[] numberInFunction = Search.SearchNumbersInTXT(function);
             int valueOperationsInBrace = Search.SearchValueOperationsInBrace(function);
             rPN.ParseExpression(numberInFunction, function, valueOperationsInBrace);
             Console.Clear();
             foreach (object result in rPN.Calculate())
                 Console.WriteLine($"Выражение: {functionTXT}\nх = {valueX}\nРезультат равен: {result}");
-            //object[] result = rPN.Calculate().ToArray();
-            //Console.WriteLine(result);
         }
     }
 }
